d5c17667513cf35da9258d8ca76d2593e2830ebf
LinuxUser255 <atnl@protonmail.com>
2026-07-10 00:16:05 -0400

    editing the banner

--- src/banner.rs
+++ src/banner.rs
@@ -1,20 +1,14 @@
 /// Prints the LinuxSecure banner
 /// Displayed at the start of every program execution
-pub fn print_banner() {
-    println!(
-        r#"
-
-                    C O M M A N D    M E N U
-    ════════════════════════════════════════════════════
-           Run Shell Scripts, Commands & other code from one place
-
-"#
-    );
-}
 
 pub fn display_banner() {
     println!(
         r#"
+                    C O M M A N D    M E N U
+          ════════════════════════════════════════════════════════
+           Run Shell Scripts, Commands & other code from one place
+
+
 ═══════════════════════════════════════════════════════════════════════════════
 
 ▶ Select an option:
